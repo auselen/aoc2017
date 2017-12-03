a9b4740096c60f8d532d027a9649f2f10f421239
auselen <auselen@gmail.com>
2017-12-03 17:04:43 +0100

    d2

---
+++ day2/d2.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::num::Wrapping;
+
+fn main() {
+    let mut f = File::open("input").expect("err");
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).expect("err");
+    let sum =
+    buffer.split("\n")
+    .filter(|i| !i.is_empty())
+    .map(
+        |s| s.split("\t")
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect()
+    )
+    .map(|v: Vec<i32>| -> i32 {
+        v.iter().max().unwrap() - v.iter().min().unwrap()
+    })
+    .fold(0, |sum, i| sum + i   );
+    println!("{}", sum);
+}
